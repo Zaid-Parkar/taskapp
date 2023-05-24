03fdfcc2752a86d9b0fe1c8c969fad37a6242dc0
Zaid Parkar <zaids.parkar@gmail.com>
2023-05-24 21:01:30 +0530

    User login & logout

--- app/Config/Services.php
+++ app/Config/Services.php
@@ -17,7 +17,7 @@ use CodeIgniter\Config\BaseService;
  * method format you should use for your service methods. For more examples,
  * see the core Services file at system/Config/Services.php.
  */
-class Services extends BaseService
+//class Services extends BaseService
 {
     /*
      * public static function example($getShared = true)
@@ -29,4 +29,17 @@ class Services extends BaseService
      *     return new \CodeIgniter\Example();
      * }
      */
+}
+class Services extends BaseService
+{
+
+	public static function auth($getShared = true)
+	{
+	    if ($getShared)
+	    {
+	        return static::getSharedInstance('auth');
+	    }
+	
+        return new \App\Libraries\Authentication;
+	}
 }
--- app/Entities/User.php
+++ app/Entities/User.php
@@ -2,6 +2,10 @@
 
 namespace App\Entities;
 
-class User extends \CodeIgniter\Entity\Entity
+class User extends \CodeIgniter\Entity
 {
+    public function verifyPassword($password)
+    {    
+        return password_verify($password, $this->password_hash);
+    }
 }
